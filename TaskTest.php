681019fb5a3720b4fd3a6701ebe1043868d885b7
Augusto Pascutti <augusto@phpsp.org.br>
2012-06-22 19:07:48 -0300

    Corrige chamada de metodo na classe Task
    Antes a chamada de metodos estava sendo feita na Suite de
    testes (), trocado pelo objeto da tarefa ()

--- TaskTest.php
+++ TaskTest.php
@@ -11,4 +11,22 @@ class TaskTest extends PHPUnit_Framework_TestCase
         $this->assertEquals($title, $task->getTitle());
         $this->assertEquals($title, (string) $task);
     }
+
+    public function testId()
+    {
+        $task = new SfCon\Task();
+        $id   = 1;
+        $task->setId($id);
+        $this->assertEquals($id, $task->getId());
+    }
+
+    public function testDone()
+    {
+        $task = new SfCon\Task();
+        $this->assertFalse($task->isDone());
+        $task->setDone(); // Default: true
+        $this->assertTrue($task->isDone());
+        $task->setDone(false);
+        $this->assertFalse($task->isDone());
+    }
 }
