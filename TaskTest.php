394b0fa38f5f8b4151e157524b68caca136aebb3
Augusto Pascutti <augusto@phpsp.org.br>
2012-06-22 21:44:19 -0300

    Refactor: Criado setUp() para teste
    * Criada fixture de banco (PDO)
    * Criada Fixture da Task

--- TaskTest.php
+++ TaskTest.php
@@ -3,47 +3,51 @@ require 'SfCon/Task.php';
 
 class TaskTest extends PHPUnit_Framework_TestCase
 {
-    public function testSetterGetterForTitle()
-    {
-        $task  = new SfCon\Task;
-        $title = 'Teste';
-        $task->setTitle($title);
-        $this->assertEquals($title, $task->getTitle());
-        $this->assertEquals($title, (string) $task);
-    }
+    protected $fixture;
+    protected $pdo;
 
-    public function testSetterGetterForId()
+    public function setUp()
     {
-        $task = new SfCon\Task();
-        $id   = 1;
-        $task->setId($id);
-        $this->assertEquals($id, $task->getId());
-    }
-
-    public function testSetterGetterForDone()
-    {
-        $task = new SfCon\Task();
-        $this->assertFalse($task->isDone());
-        $task->setDone(); // Default: true
-        $this->assertTrue($task->isDone());
-        $task->setDone(false);
-        $this->assertFalse($task->isDone());
-    }
-
-    public function testInsert()
-    {
-        $pdo = new Pdo('sqlite::memory:');
-        $pdo->exec('CREATE TABLE tasks (
+        $this->pdo     = new Pdo('sqlite::memory:');
+        $this->fixture = new SfCon\Task($this->pdo);
+        $this->pdo->exec('CREATE TABLE IF NOT EXISTS tasks (
             id INTEGER PRIMARY KEY,
             title TEXT,
             done INTEGER
         )');
-        $task = new SfCon\Task($pdo);
+    }
+
+    public function testSetterGetterForTitle()
+    {
+        $title = 'Teste';
+        $this->fixture->setTitle($title);
+        $this->assertEquals($title, $this->fixture->getTitle());
+        $this->assertEquals($title, (string) $this->fixture);
+    }
+
+    public function testSetterGetterForId()
+    {
+        $id   = 1;
+        $this->fixture->setId($id);
+        $this->assertEquals($id, $this->fixture->getId());
+    }
+
+    public function testSetterGetterForDone()
+    {
+        $this->assertFalse($this->fixture->isDone());
+        $this->fixture->setDone(); // Default: true
+        $this->assertTrue($this->fixture->isDone());
+        $this->fixture->setDone(false);
+        $this->assertFalse($this->fixture->isDone());
+    }
+
+    public function testInsert()
+    {
         $expectId = 1;
-        $task->setTitle('Test');
-        $task->insert(); // Inserts must define ID into the object
-        $this->assertEquals($expectId, $task->getId());
-        $st = $pdo->prepare('SELECT id FROM tasks');
+        $this->fixture->setTitle('Test');
+        $this->fixture->insert(); // Inserts must define ID into the object
+        $this->assertEquals($expectId, $this->fixture->getId());
+        $st = $this->pdo->prepare('SELECT id FROM tasks');
         $st->execute();
         $all = $st->fetchAll(\PDO::FETCH_ASSOC);
         $this->assertEquals(1, count($all));
